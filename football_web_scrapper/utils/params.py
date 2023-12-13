838c9ef1be0c00f8c63800ba5521f85580db14a9
duong nguyen <melody.duong.nguyen@gmail.com>
2023-12-13 13:11:45 +0900

    add params file in folder utils

---
+++ football_web_scrapper/utils/params.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+
+##################  CONSTANTS  #####################
+CURRENT_DIRECTORY = os.getcwd()
+CSV_DIRECTORY = os.path.join(os.getcwd(),'csv')
+URL = "https://www.adamchoi.co.uk/overs/detailed"
